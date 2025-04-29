764ca612217ae75dbaa37352356a84d8dec377c4
rodrigoo-r <2nw53j32643y@gmail.com>
2025-04-29 11:16:40 -0500

    Refactor main.cpp to define program metadata and improve help display

--- main.cpp
+++ main.cpp
@@ -12,9 +12,16 @@
    conditions; type `fluent l -f` for details.
 */
 
+// Macros
+#define PROGRAM_NAME "fluentc"
+#define PROGRAM_DESC "The Fluent Language Compiler"
+#define PROGRAM_VERSION "1.0.0"
+
 #include <iostream>
 
 #include <map>
+
+#include "algorithm/cli/help_generator.h"
 #include "args/argv_impl.h"
 #include "args/flag/flag.h"
 
@@ -45,23 +52,32 @@ std::map<std::string, Flag> get_flags()
         0,
     };
 
+    flags["help"] = Flag{
+        "",
+        "Displays this menu",
+        "h",
+        0,
+    };
+
     return flags;
 }
 
 int main(const int argc, const char** argv)
 {
     // Print copyright messages
-    cout << "fluentc - The Fluent Language Compiler" << endl;
+    cout << PROGRAM_NAME << "- " << PROGRAM_DESC << endl;
     cout << "Copyright (c) 2025 Rodrigo R. & All Fluent Contributors" << endl;
     cout << endl;
 
+    // Get the flags
+    const auto flags = get_flags();
     // Parse argv
-    const ArgvImpl parsing_result = parse(argc, argv, get_flags());
+    const ArgvImpl parsing_result = parse(argc, argv, flags);
 
     // Handle errors
     if (!parsing_result.is_success())
     {
-
+        print_help(PROGRAM_NAME, flags);
     }
 
     return 0;
