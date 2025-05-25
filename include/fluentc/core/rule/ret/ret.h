aa31b6f57cf29313acc3d5ce2389199fd2f07b18
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-25 00:00:28 -0500

    Refactor ret.h: move process_ret function into fluent::compiler::rule namespace

--- include/fluentc/core/rule/ret/ret.h
+++ include/fluentc/core/rule/ret/ret.h
@@ -19,35 +19,37 @@
 #ifndef FLUENTC_RULE_RET_H
 #define FLUENTC_RULE_RET_H
 
-inline void process_ret(
-    llvm::IRBuilder<> &builder,
-    const std::shared_ptr<fluent::parser::AST> &child,
-    const ankerl::unordered_dense::map<std::string_view, fluent::compiler::variable::Variable> &variables,
-    const ankerl::unordered_dense::map<std::string_view, llvm::GlobalVariable *> &refs
-)
+namespace fluent::compiler::rule
 {
-    // Get the children
-    const auto &children = fluent::util::try_unwrap(child->children);
-
-    // Check if the return type is void
-    if (children.empty())
+    inline void process_ret(
+        llvm::IRBuilder<> &builder,
+        const std::shared_ptr<fluent::parser::AST> &child,
+        const ankerl::unordered_dense::map<std::string_view, fluent::compiler::variable::Variable> &variables,
+        const ankerl::unordered_dense::map<std::string_view, llvm::GlobalVariable *> &refs
+    )
     {
-        // Add a void return instruction
-        builder.CreateRetVoid();
-        return;
+        // Get the children
+        const auto &children = fluent::util::try_unwrap(child->children);
+
+        // Check if the return type is void
+        if (children.empty())
+        {
+            // Add a void return instruction
+            builder.CreateRetVoid();
+            return;
+        }
+
+        // Get the expression
+        const auto &expr = children[0];
+        fluent::util::assert_eq(expr->rule, fluent::parser::Expression);
+
+        // Get the identifier
+        const auto &identifier = children[1];
+        fluent::util::assert_eq(identifier->rule, fluent::parser::Identifier);
+        const auto id_val = fluent::util::try_unwrap(identifier->value);
+
+        // Create a return instruction
+        builder.CreateRet(find_value(variables, refs, id_val));
     }
-
-    // Get the expression
-    const auto &expr = children[0];
-    fluent::util::assert_eq(expr->rule, fluent::parser::Expression);
-
-    // Get the identifier
-    const auto &identifier = children[1];
-    fluent::util::assert_eq(identifier->rule, fluent::parser::Identifier);
-    const auto id_val = fluent::util::try_unwrap(identifier->value);
-
-    // Create a return instruction
-    builder.CreateRet(find_value(variables, refs, id_val));
 }
-
 #endif //FLUENTC_RULE_RET_H
