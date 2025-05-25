2e777895aa6b61e5d3908e15a47a313979a17019
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-24 19:01:44 -0500

    Enhance process_call function: add context parameter and implement struct initialization logic for constructors

--- include/fluentc/core/rule/call/call.h
+++ include/fluentc/core/rule/call/call.h
@@ -27,11 +27,14 @@ namespace fluent::compiler::rule
 {
     inline llvm::Value *process_call(
         const llvm::Module *module,
+        llvm::LLVMContext &context,
         llvm::IRBuilder<> &builder,
         const std::shared_ptr<parser::AST> &call,
         const ankerl::unordered_dense::map<std::string_view, variable::Variable> &variables,
         const ankerl::unordered_dense::map<std::string_view, llvm::GlobalVariable *> &refs,
-        const bool is_construct = false
+        stats::CompileTimeStats &ct_stats,
+        const bool is_construct = false,
+        const bool is_declaration
     )
     {
         // Get the children
@@ -55,7 +58,30 @@ namespace fluent::compiler::rule
 
         if (is_construct)
         {
-            // TODO!
+            // Get the struct type
+            llvm::Type *struct_ty = llvm::StructType::getTypeByName(context, name->value->data());
+
+            // Create a new alloca instruction for the struct
+            llvm::AllocaInst *struct_ptr = builder.CreateAlloca(struct_ty, nullptr, ct_stats.request_addr());
+
+            // Add all fields
+            for (size_t i = 1; i < children.size(); i++)
+            {
+                // Get the identifier
+                const auto id = children[i];
+                util::assert_eq(variables.contains(id->value.value()), true);
+
+                // Get the variable
+                const auto var = variables.at(id->value.value());
+
+                // Get the filed
+                llvm::Value *field = builder.CreateStructGEP(struct_ty, struct_ptr, i - 1, ct_stats.request_addr());
+
+                // Store the information
+                builder.CreateStore(var.value, field);
+            }
+
+            return struct_ptr;
         }
 
         // Create a function call
